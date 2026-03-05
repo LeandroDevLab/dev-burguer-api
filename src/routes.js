2d2e4316eb0670856e33fcfbd428b92fdada1c76
LeandroDevLab <prof.oliota@gmail.com>
2026-03-05 19:13:53 -0300

    feat(user): implementei registro de UserController

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,8 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
-import User from './app/models/User.js';
+import UserController from './app/controllers/UserController.js';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = {
-    id: v4(),
-    name: 'Henrique',
-    email: 'henrique@gmail.com',
-    password_hash: '123123',
-    admin: false,
-  };
-
-  await User.create(user);
-
-  res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
